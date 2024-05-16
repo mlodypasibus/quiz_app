ddb2b281c0c735d5d718df5baaa292ab604026d2
mlodypasibus <jakubjedryszczak01@gmail.com>
2024-05-17 00:13:32 +0200

    17,05

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
 
 
 class QuestionsSummary extends StatelessWidget{
-  const QuestionsSummary({super.key, required this.summaryData});
+  const QuestionsSummary(this.summaryData, {super.key,});
   final List<Map<String, Object>> summaryData;
 
   @override
@@ -11,7 +12,13 @@ class QuestionsSummary extends StatelessWidget{
       children: summaryData.map((data){
         return Row(
           children: [
-            //Text(data['question_index'])
+            Text(((data['question_index'] as int ) + 1).toString()),
+            Column(children: [
+              Text(data['question'] as String),
+              const SizedBox(height: 5,),
+              Text(data['user_answer'] as String),
+              Text(data['correct_answer'] as String)
+            ],),
           ]
           );
       },
--- lib/result_screen.dart
+++ lib/result_screen.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/questions_summary.dart';
 
 class ResultScreen extends StatelessWidget{
   const ResultScreen({
@@ -35,7 +37,7 @@ class ResultScreen extends StatelessWidget{
         children: [
           const Text('You answered X out of Y questions correctly'),
           const SizedBox(height: 30,),
-          const Text('List of answers and questions...'),
+          QuestionsSummary(getSummaryData()),
           const SizedBox(height: 30,),
           TextButton(
             onPressed: () {}, 
